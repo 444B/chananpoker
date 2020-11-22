679a7c943b6ad363a1ce5b7c909441ab34e5bd06
Blue42 <444b@protonmail.ch>
2020-11-22 20:34:48 +0200

    beginning

---
+++ main.py
@@ -0,0 +1,45 @@
+from random import shuffle
+
+game = True
+
+# Card park set
+card_pack = ['Ace Hearts', '2 Hearts', '3 Hearts', '4 Hearts', '5 Hearts', '6 Hearts', '7 Hearts', '8 Hearts',
+             '9 Hearts', '10 Hearts', 'Jack Hearts', 'Queen Hearts', 'King Hearts', 'Ace Clubs', '2 Clubs',
+             '3 Clubs', '4 Clubs', '5 Clubs', '6 Clubs', '7 Clubs', '8 Clubs', '9 Clubs', '10 Clubs', 'Jack Clubs',
+             'Queen Clubs', 'King Clubs', 'Ace Diamonds', '2 Diamonds', '3 Diamonds', '4 Diamonds', '5 Diamonds',
+             '6 Diamonds', '7 Diamonds', '8 Diamonds', '9 Diamonds', '10 Diamonds', 'Jack Diamonds', 'Queen Diamonds',
+             'King Diamonds', 'Ace Clubs', '2 Clubs', '3 Clubs', '4 Clubs', '5 Clubs', '6 Clubs', '7 Clubs', '8 Clubs',
+             '9 Clubs', '10 Clubs', 'Jack Clubs', 'Queen Clubs', 'King Clubs', ]
+# Number of Players
+num_players =
+
+# shuffle card pack
+shuffle(card_pack)
+
+# TODO
+# Deal card ti players 1 and 2, alternatively
+
+# Game setup, including initial card choice
+# 1 = face up, 0 = face down
+card_state = 1
+rounds = 4
+options = 5
+
+# Game function
+choice = input("where do you want to put your card?")
+
+
+class player:
+    # Player names
+    def __init__(self):(self, name):
+        self.name = input('Player' + str(num_players) + ' what is your name?')
+
+    def hand(self):
+
+# TODO
+# Display Board
+
+
+# winner varaibe
+# Format Board
+
